635dbfbe7adc1db45019a213f6b4374fd44d0efd
aman kumar <aman.k@ninestars.in>
2025-01-30 09:09:15 +0530

    v0.1.9 modified socket_message(delete older message more than 100)

--- websocket_app/socket_message.py
+++ websocket_app/socket_message.py
@@ -1,23 +1,43 @@
-class SocketMessage:
-    def __init__(self):
-        self._messages = {}
+import time
+from threading import Lock
 
-    def add_message(self, room_id, room_name,user_id, message):
-        if room_id not in self._messages:
-            self._messages[room_id] = {
-                "room_name": room_name,
-                "messages": []
-            }
-        self._messages[room_id]["messages"].append({
-            'user_id': user_id,
-            'message': message
-        })
+class SocketMessage:
+    def __init__(self, max_messages_per_room=100):
+        self._messages = {}
+        self._lock = Lock()  
+        self.max_messages_per_room = max_messages_per_room  
+
+    def add_message(self, room_id, room_name, user_id, message):
+        with self._lock:  
+            if room_id not in self._messages:
+                self._messages[room_id] = {
+                    "room_name": room_name,
+                    "messages": []
+                }
+
+            timestamp = time.time()
+            self._messages[room_id]["messages"].append({
+                'user_id': user_id,
+                'message': message,
+                'timestamp': timestamp
+            })
+
+            if len(self._messages[room_id]["messages"]) > self.max_messages_per_room:
+                self._messages[room_id]["messages"].pop(0)  
 
     def get_messages(self, room_id):
-        return self._messages.get(room_id, {"room_name": "", "messages": []})
+        with self._lock:  
+            room_data = self._messages.get(room_id, {"room_name": "", "messages": []})
+            room_data["messages"] = sorted(room_data["messages"], key=lambda x: x['timestamp'])
+            return room_data
 
     def clear_room_messages(self, room_id):
-        if room_id in self._messages:
-            self._messages[room_id]["messages"].clear()
+        with self._lock:  
+            if room_id in self._messages:
+                self._messages[room_id]["messages"].clear()
 
-socket_message_manager = SocketMessage()
+    def get_all_rooms(self):
+        with self._lock: 
+            return list(self._messages.keys())
+
+socket_message_manager = SocketMessage(max_messages_per_room=100)
